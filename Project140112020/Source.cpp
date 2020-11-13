ce201f5a179fb0a9242a82ebc1823e48eb17fa6b
ASUS <ASUS@LAPTOP-2JE99TTR>
2020-11-14 01:19:48 +0700

    1

--- Project140112020/Source.cpp
+++ Project140112020/Source.cpp
@@ -2,48 +2,47 @@
 #include<conio.h>
 #include<stdlib.h>
 
-struct phonebook getData();
-void printData(struct phonebook);
+struct book getData();
+void printData(struct book);
 
-struct phonebook {
+struct book {
     char name[11];
-    char tel_num[11];
+    char age[11];
 };
 
 int main()
 {
     int i = 0, num = 2;
     char tmp[10];
-    struct phonebook phb[5];
+    struct book age[10];
     system("cls");
 
     do {
-        printf("How many number do you want to add tel.no (Maximum:5) : ");
+        printf("How many name do you want to know age (Maximum:10) : ");
         gets_s(tmp);
-    } while (atoi(tmp) < 0 || atoi(tmp) > 5);
+    } while (atoi(tmp) < 0 || atoi(tmp) > 10);
     num = atoi(tmp);
     printf("Please add data\n");
     for (i = 0; i < num; i++) {
-        phb[i] = getData();
+        age[i] = getData();
     }
     printf("\nData list\n");
     for (i = 0; i < num; i++) {
-        printData(phb[i]);
+        printData(age[i]);
     }
-    printf("Already Add !!!\n");
     _getch();
 }
 
-struct phonebook getData() {
-    struct phonebook phone;
+struct book getData() {
+    struct book input;
 
     printf("Enter name : ");
-    gets_s(phone.name);
-    printf("Enter tel number : ");
-    gets_s(phone.tel_num);
-    return phone;
+    gets_s(input.name);
+    printf("Enter age : ");
+    gets_s(input.age);
+    return input;
 }
 
-void printData(struct phonebook pb) {
-    printf("%s\t%s\n", pb.name, pb.tel_num);
+void printData(struct book input) {
+    printf("%s\t%s\n", input.name, input.age);
 }
